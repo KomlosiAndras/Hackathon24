d4e46a588077e56f9909dbc79689e08b8492d8b0
KomlosiAndras <91115916+KomlosiAndras@users.noreply.github.com>
2024-05-25 19:37:03 +0200

    palindrome checker
    Signed-off-by: KomlosiAndras <91115916+KomlosiAndras@users.noreply.github.com>

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,17 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+def remove_non_alphanumeric_chars(string):
+  return ''.join([i for i in string if i.isalnum()])
 
-print(input)
+def number_of_unique_chars(string):
+  return len(set(string))
+
+def is_palindrome(string):
+  return string == string[::-1]
+
+with open('./input.txt', 'r') as f:
+  lines = f.readlines()
+  for i in lines:
+    string = remove_non_alphanumeric_chars(i.lower())
+    if is_palindrome(string):
+      print(f"YES, {number_of_unique_chars(string)}")
+    else: 
+      print("NO, -1")
